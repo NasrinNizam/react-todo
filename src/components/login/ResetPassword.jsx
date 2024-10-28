7e237cdf9708db69e6516766d5714d865ab573da
Nasrin Sultana <168544785+NasrinNizam@users.noreply.github.com>
2024-10-28 21:08:27 +0600

    reset pass change

--- src/components/login/ResetPassword.jsx
+++ src/components/login/ResetPassword.jsx
@@ -32,16 +32,16 @@ export const ResetPassword = () => {
 
     }
   return (
-    <div>
-               <div className="section w-full h-screen flex justify-center items-center">
+    <>
+      <div className="section w-full h-screen flex justify-center items-center">
        <div className='w-[300px] md:w-[400px] bg-[#B9E5E8] flex flex-col justify-center items-center gap-5 rounded-lg p-10'>
-          <div className="flex justify-center items-center gap-3 flex-col md:flex-row">
-            <input onClick={handleEmail} className='p-2 rounded-lg w-[250px] ' type="email" placeholder='Give your email' />
-            <button onClick={handleSubmit}  className='px-5 py-2 bg-white rounded-lg'>Send</button>
-          </div>
-        <Link className='px-5 py-2 bg-white rounded-lg' to='/'>Home</Link>
-    </div>
+              <div className="flex justify-center items-center gap-3 flex-col md:flex-row">
+                 <input onClick={handleEmail} className='p-2 rounded-lg w-[250px] ' type="email" placeholder='Give your email' />
+                 <button  onClick={handleSubmit} type='submit' className='px-3 py-2 w-[60px] text-black bg-white rounded-lg'>Send</button>
+              </div>
+          <Link className='px-5 py-2 bg-white rounded-lg' to='/'>Home</Link>
+         </div>
        </div>
-    </div>
+    </>
   )
 }
